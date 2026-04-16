71cb9a8ebfebe8bc52db0ac5b9d6f517332c972b
Julian <julian.ocampo1219@gmail.com>
2026-04-16 19:54:03 -0300

    Clase Marca + Holaa bt

---
+++ TP-WinForm-Programacion-3/TP-WinForm-Programacion3/Marca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_Programacion3
+{
+    internal class Marca
+    {
+        private int Id;
+        private string Descripcion;
+       
+    }
+}
