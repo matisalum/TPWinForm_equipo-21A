b3eedcf0facfb28ef39eea7bd3ac0d319915aa0a
Javier Altamirano <javi.alt87@gmail.com>
2026-04-16 20:07:29 -0300

    calse categoria

---
+++ TP-WinForm-Programacion-3/TP-WinForm-Programacion3/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_Programacion3
+{
+    internal class Categoria
+    {
+        private int Id;
+        private string Descripcion;
+    }
+}
